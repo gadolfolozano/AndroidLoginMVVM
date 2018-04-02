6dfd66b2022407c0817bced5b301b71e2847976c
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2018-04-02 18:11:07 -0500

    add jacoco for reporting unit tests

--- app/src/test/java/pe/gadolfolozano/mvvmlogin/ExampleUnitTest.java
+++ app/src/test/java/pe/gadolfolozano/mvvmlogin/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package pe.gadolfolozano.mvvmlogin;
 
 import org.junit.Test;
 
+import pe.gadolfolozano.mvvmlogin.ui.model.BaseModelLiveData;
+import pe.gadolfolozano.mvvmlogin.util.Constants;
+
 import static org.junit.Assert.*;
 
 /**
@@ -14,4 +17,18 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void testBaseModelLiveData() throws Exception {
+        BaseModelLiveData baseModelLiveData = new BaseModelLiveData();
+        baseModelLiveData.setErrorMessage("error");
+        baseModelLiveData.setSuccesfull(false);
+
+        assertEquals(false, baseModelLiveData.isSuccesfull());
+        assertNotNull(baseModelLiveData);
+        assertNotNull(baseModelLiveData.getErrorMessage());
+        assertNull(baseModelLiveData.getData());
+        assertEquals(0, baseModelLiveData.getStatusCode());
+    }
+
 }
